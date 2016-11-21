903f0195df368f3c62704875cb71c4627c2c662b
Justin Moen <jamoen7@gmail.com>
2016-11-22 02:21:50 +0700

    Remove shadow import

--- native/RoundRobin.js
+++ native/RoundRobin.js
@@ -13,12 +13,9 @@ import {
 } from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
 
-import EditorContainer from './react/Container.js';
-
 // import Test from './react/Test.js';
+import EditorContainer from './react/Container.js';
 import Community from './react/Community.js';
-import Swipe from './react/Swipe.js';
-
 
 export default class RoundRobin extends Component {
   render() {
