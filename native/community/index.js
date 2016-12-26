94e31df40afb4bb336e847fb715c567e0684d902
Justin Moen <jamoen7@gmail.com>
2016-12-26 21:00:20 +0800

    Eslint config

--- native/community/index.js
+++ native/community/index.js
@@ -2,5 +2,4 @@ import * as actionTypes from './actionTypes'
 import * as actions from './actions'
 import * as components from './components'
 
-
 export default { actions, actionTypes, components }
