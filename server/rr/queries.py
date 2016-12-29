f8570dcf0444b66a43a93275114775af2970de67
Justin Moen <jamoen7@gmail.com>
2016-12-29 13:12:44 +0800

    Fix calling screwup

--- server/rr/queries.py
+++ server/rr/queries.py
@@ -26,7 +26,7 @@ def construct_feed():
         .order_by(Composition.created_at.desc())
 
 
-def create_composition(*, prompt_id, created_at, user_id, body):
+def create_composition(created_at=datetime.now(), *, prompt_id, user_id, body):
     c = Composition(
         user_id=user_id,
         created_at=created_at,
