6a47c18cb00f1033381af91622b24e897fa50d7f
Justin Moen <jamoen7@gmail.com>
2017-03-08 14:04:41 -0600

    react-native-config integration

--- backend/rr/secrets.py
+++ backend/rr/secrets.py
@@ -4,19 +4,16 @@ import os
 class ProductionSecrets:
     DEBUG=False
     SECRET_KEY='5(15ds+i2+%ik6z&!yer+ga9m=e%jcqiz_5wszg)r-z!2--b2d'
-    DB_USER = 'postgres'
-    DB_NAME='postgres'
-    DB_PASS='postgres'
-    DB_SERVICE='postgres'
-    DB_PORT=5432
+    DB_USER = 'root'
+    DB_PASS='password'
+    DB_SERVICE='/prod?unix_socket=/cloudsql/round-robin-prod:us-central1:app-engine'
 
 class DevelopmentSecrets:
     DEBUG=True
     SECRET_KEY='5(15ds+i2+%ik6z&!yer+ga9m=e%jcqiz_5wszg)r-z!2--b2d'
-    DB_USER = 'postgres'
-    DB_NAME='postgres'
-    DB_PASS='postgres'
-    DB_SERVICE='postgres'
+    DB_USER = 'user'
+    DB_PASS='password'
+    DB_SERVICE='localhost/round_robin_dev'
 
 class StagingSecrets:
     DEBUG=False
---
+++ native/config.js
@@ -0,0 +1,22 @@
+import Config from 'react-native-config'
+
+console.log(Config)
+
+const SIMPLE_INTEGER_REGEX = /^\d+$/
+
+function cast(configValue) {
+  if (['true', 'false'].indexOf(configValue) !== -1) {
+    return Boolean(configValue)
+  }
+  if (SIMPLE_INTEGER_REGEX.test(configValue)) {
+    return parseInt(configValue)
+  }
+  return configValue
+}
+
+const config = Object.keys(Config).reduce((acc, key) => {
+  acc[key] = cast(Config[key])
+  return acc
+}, {})
+
+export default config
