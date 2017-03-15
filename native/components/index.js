6fcb976f6ab97abf62e37ef2d7e6811ac6cb8444
Justin Moen <jamoen7@gmail.com>
2017-03-15 04:32:33 -0500

    Published sub-tab has list-view

--- native/components/index.js
+++ native/components/index.js
@@ -1,5 +1,6 @@
 import { default as FadingView } from './FadingView'
 import { default as TabIcon } from './TabIcon'
 import { default as CompositionList } from './CompositionList'
+import { default as Published } from './Published'
 
-export default { FadingView, TabIcon, CompositionList }
+export default { FadingView, TabIcon, CompositionList, Published }
