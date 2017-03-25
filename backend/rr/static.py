fa4cbe143f612cd01a9064a66deeb9b7a90eba77
Justin Moen <jamoen7@gmail.com>
2017-03-24 20:35:34 -0500

    Demos page for YC

--- backend/rr/static.py
+++ backend/rr/static.py
@@ -1,12 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, render_template_string
 
 static = Blueprint('serve', __name__, template_folder='build')
-#  static = Blueprint('serve', __name__, template_folder='../../web/build')
 
 
 @static.route('/')
 def index():
     return render_template('index.html')
+
+
+@static.route('/demos')
+def demos():
+    return render_template_string("""
+    <h3>Up-to-date demos available at the following locations</h3>
+    <ul>
+        <li><a href='http://roundrob.in/login'>Web</a></li>
+        <li><a href='https://play.google.com/store/apps/details?id=com.roundrobin&hl=en'>Android</a></li>
+        <li>Invitation to iOS app available by emailing <a href='mailto:jamoen7@gmail.com'>jamoen7@gmail.com</a>. FYI it's nearly the same as Android version.</li>
+    </ul>""")
 
 
 @static.route('/<path:path>')
